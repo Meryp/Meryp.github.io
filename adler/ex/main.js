164e03a94e0b1ff516297c084db0d3a205552439
Meryp <48288935+Meryp@users.noreply.github.com>
2019-03-27 16:53:29 +0100

    Etappe

---
+++ adler/ex/main.js
@@ -0,0 +1,22 @@
+//alert("Hallo Welt");
+
+const div = document.getElementByld("map")
+const breite = div.getAttribute("data-lat");
+const laenge = div.getAttribute("data-Ing");
+const titel = div.getAttribute("data-title");
+
+//console.log(breite,laenge,titel)
+
+//Karte initialisiren
+
+let karte = L.map("map");
+//console.log(karte);
+//aus Ausschnitt zoomen
+
+karte.setView([breite,laenge],13);
+
+//openstreetmap hinzufügen L.tileLayer("http://{s}.tile.osm.org./{z}/{x}/{y}.png").addTo(karte);
+
+//Positionsmarker hinzufügen let pin = L.marker([breite,laenge]).addTo(karte);
+
+//Popup zum Pin hängen pin.bindPopup(titel).openPopup();
