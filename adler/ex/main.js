bc0e8057b5f193868e852798233c6e2af80458b1
Meryp <48288935+Meryp@users.noreply.github.com>
2019-03-27 18:15:53 +0100

    n

--- adler/ex/main.js
+++ adler/ex/main.js
@@ -22,19 +22,46 @@ karte.setView([47.2,11.2],
 
 //Popup zum Pin hängen pin.bindPopup(titel).openPopup();
 
-const blick1 = { 
-    kunde: "Wilder Kaiser", 
-    standort:"Gruttenhütte"
-    seehoehe: 1640,
-    lat: 47.55564,
-    lng: 12.31861
-};
-
-
 let pin3 = L.marker(
     [blick1.lat, blick1.lng]
 ).addTo(karte);
-pin3.bindPopup(blick1.standort);
+
+const adlerblicke = [
+    {
+    kunde: "Wilder Kaiser", 
+    standort:"Gruttenhütte",
+    seehoehe: 1640,
+    lat: 47.55564,
+    lng: 12.31861
+},
+{
+    kunde: "Bergbahn Scheffau",
+    standort: "Brandstadl",
+    seehoehe: 1640,
+    lat: 47.49111,
+    lng: 12.24778
+},
+{
+    kunde: "Lechtal Tourismus ",
+    standort: "Sonnalm Jöchelspitze",
+    seehoehe: 1640,
+    lat: 47.41028,
+    lng: 10.24778
+}
+];
+
+for (let blick of adlerblicke) {
+    console.log(blick);
+    let blick = L.marker( 
+        [blick.lat,blick.lng]
+    ).addTo(karte);
+    blickpin.bindPopup(
+        `<h1>Standort ${blick1.standort}</h1>
+        <p>Höhe: ${blick1.seehoehe} m</p>
+        <em>Kunde: ${blick1.kunde}</em>`
+        );
+}
+
 
 
 
