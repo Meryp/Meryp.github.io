ffa7fe8f589ec1eedc95446d02685c33e15954ff
Meryp <48288935+Meryp@users.noreply.github.com>
2019-03-27 17:39:58 +0100

    t

--- adler/ex/main.js
+++ adler/ex/main.js
@@ -13,10 +13,29 @@ let karte = L.map("map");
 //console.log(karte);
 //aus Ausschnitt zoomen
 
-karte.setView([breite,laenge],13);
+karte.setView([47.2,11.2],
+    9);
 
 //openstreetmap hinzufügen L.tileLayer("http://{s}.tile.osm.org./{z}/{x}/{y}.png").addTo(karte);
 
 //Positionsmarker hinzufügen let pin = L.marker([breite,laenge]).addTo(karte);
 
 //Popup zum Pin hängen pin.bindPopup(titel).openPopup();
+
+const blick1 = { 
+    kunde: "Wilder Kaiser", 
+    standort:"Gruttenhütte"
+    seehoehe: 1640,
+    lat: 47.55564,
+    lng: 12.31861
+};
+
+
+let pin3 = L.marker(
+    [blick1.lat, blick1.lng]
+).addTo(karte);
+pin3.bindPopup(blick1.standort);
+
+
+
+
