5d4def878e450a93777b73e0c27a2aefed2f6616
Meryp <48288935+Meryp@users.noreply.github.com>
2019-04-02 21:44:40 +0200

    map not found

--- adler/main.js
+++ adler/main.js
@@ -1,6 +1,6 @@
 //alert("Hallo Welt");
 
-const div = document.getElementByld("map")
+const div = document.getElementById("map");
 const breite = div.getAttribute("data-lat");
 const laenge = div.getAttribute("data-lng");
 const titel = div.getAttribute("data-title");
