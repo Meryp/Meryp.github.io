033a9ce89a3fb62fccce0c294ab6330ba16d9a56
Meryp <48288935+Meryp@users.noreply.github.com>
2019-04-02 09:58:18 +0200

    aktuell

--- nz/main.js
+++ nz/main.js
@@ -1,22 +1,28 @@
 //alert("Hallo Welt");
 
-const div = document.getElementByld("map")
+const div = document.getElementById("map");
 const breite = div.getAttribute("data-lat");
 const laenge = div.getAttribute("data-Ing");
 const titel = div.getAttribute("data-title");
 
-//console.log(breite,laenge,titel)
+console.log("Breite=",breite)
 
 //Karte initialisiren
 
 let karte = L.map("map");
+
 //console.log(karte);
 //aus Ausschnitt zoomen
 
-karte.setView([breite,laenge],13);
+karte.setView(
+    [breite,laenge],
+    13);
 
-//openstreetmap hinzufügen L.tileLayer("http://{s}.tile.osm.org./{z}/{x}/{y}.png").addTo(karte);
+//openstreetmap hinzufügen 
+L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
 
-//Positionsmarker hinzufügen let pin = L.marker([breite,laenge]).addTo(karte);
+//Positionsmarker hinzufügen 
+let pin = L.marker([breite,laenge]).addTo(karte);
 
-//Popup zum Pin hängen pin.bindPopup(titel).openPopup();
+//Popup zum Pin hängen 
+pin.bindPopup(titel).openPopup();
